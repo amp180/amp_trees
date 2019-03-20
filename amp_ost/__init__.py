4806d69d34eba9a3447a5661407b1ae0ad8c787d
Adam Pegman <adam@pegman.space>
2019-03-20 18:23:49 +0000

    commit

--- amp_ost/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from amp_ost._ext import *
-except:
-    pass
-
-__version__ = "0.0.1a"
--- setup.py
+++ setup.py
@@ -1,14 +1,42 @@
-from setuptools import setup, Extension, find_packages
-from amp_ost import __version__
+from setuptools import setup, find_packages
+import builtins
+import glob
+
+builtins.SETUP = True
+from amp_ostree import __version__
 
 
-extension = Extension("amp_ost._ext",
-                           sources=["ext_source/ost.c"],
-                           )
+# Check if cython is intalled.
+try:
+    from Cython.Build import cythonize
+    from Cython.Distutils.extension import Extension
+    from Cython.Distutils import build_ext
+except ImportError:
+    from setuptools import Extension
+    USING_CYTHON = False
+else:
+    USING_CYTHON = True
+
+
+# Build from .pyx files if cython is installed, otherwise assume installing from sdist and c files exist.
+ext = '.pyx' if USING_CYTHON else '.c'
+treeset = Extension("amp_ostree.treeset", ["amp_ostree/treeset"+ext])
+treemap = Extension("amp_ostree.treemap", ["amp_ostree/treemap"+ext])
+
+modules = [treeset, treemap]
+
+# generate .c files
+if USING_CYTHON:
+    modules = cythonize(modules)
+
 setup(
-    name="amp_ost",
+    name="amp_ostree",
     license="MIT",
-    ext_modules=[extension, ],
+    ext_modules=modules,
     packages=find_packages(),
-    version=__version__
+    package_data={'amp_ostree': glob.glob("amp_ostree/*.((pyx)|(c))"),}, # Include pyx and c files in sdist
+    include_package_data=False, # Don't install the pyx files with the bdist.
+    version=__version__,
+    cmdclass = {'build_ext': build_ext} if USING_CYTHON else {}
+
 )
